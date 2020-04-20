a9d46a431b35dc762e455bf9212e5f0a9fc251fd
Hektor Misplon <hektor.misplon@gmail.com>
2020-04-20 19:56:37 +0000

    Set up routing

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,32 +1,38 @@
-import React, { useState } from "react";
-import { View, Image } from "react-native";
-import { AppLoading } from "expo";
-import * as Font from "expo-font";
-import { Asset } from "expo-asset";
-import { Feather as Icon } from "@expo/vector-icons";
+import { Feather as Icon } from '@expo/vector-icons'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+import React, { useState } from 'react'
+import { View, Image } from 'react-native'
+
+import HomeScreen from './src/screens/Home'
 
 const cacheImages = images =>
   images.map(image =>
-    typeof image == "string"
+    typeof image == 'string'
       ? Image.prefetch(image)
       : Asset.fromModule(image).downloadAsync()
-  );
+  )
 
-const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font));
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font))
 
 const _loadAssetsAsync = async () => {
   const imageAssets = cacheImages([
-    "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-  ]);
+    'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+  ])
 
-  const fontAssets = cacheFonts([Icon.font]);
+  const fontAssets = cacheFonts([Icon.font])
 
-  await Promise.all([...imageAssets, ...fontAssets]);
-};
+  await Promise.all([...imageAssets, ...fontAssets])
+}
+
+const Stack = createStackNavigator()
 
 export default () => {
   // render AppLoading until all is loaded
-  [loading, setLoading] = useState(true);
+  ;[loading, setLoading] = useState(true)
   return loading ? (
     <AppLoading
       startAsync={_loadAssetsAsync}
@@ -34,8 +40,10 @@ export default () => {
       onError={console.warn}
     />
   ) : (
-    <View>
-      <Icon name="home" size={64} />
-    </View>
-  );
-};
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ client/app/src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Button from '../components/Button'
+
+export default () => (
+  <View>
+    <Button icon="home" text="Home" />
+  </View>
+)
