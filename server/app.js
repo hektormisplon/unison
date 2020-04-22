95be7535e21f89b97c926a6d573c9932175fe22f
Hektor Misplon <hektor.misplon@gmail.com>
2020-04-22 17:19:42 +0000

    Stop mongoose's use of deprecated 'useCreateIndex'

--- server/app.js
+++ server/app.js
@@ -32,6 +32,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log('Connected to database'))
   .catch(err => console.error(err))
