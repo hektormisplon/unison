fcfb049e419c2469defa3537891c487332fef808
Hektor Misplon <hektor.misplon@gmail.com>
2020-04-22 13:59:22 +0000

    Set up mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
-const express = require('express'),
-  cors = require('cors')
-
 require('dotenv/config')
+const express = require('express'),
+  mongoose = require('mongoose'),
+  cors = require('cors'),
+  cookieParser = require('cookie-parser'),
+  path = require('path')
 
 /*
-Import modules */
+Import routes */
 
 const apiRouter = require('./routes/api')
 
@@ -15,11 +17,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
 if (app.get('env') === 'development')
   app.use((err, req, res, next) => {
     res.status(err.status || 500)
     res.render('error', { message: err.message, error: err })
   })
+
+/*
+Connect to mongodb */
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error(err))
 
 /*
 Router */
