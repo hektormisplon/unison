01353087303a9ee5144f0c561e49d3a4d23f51d7
Hektor Misplon <hektor.misplon@gmail.com>
2020-04-23 23:49:33 +0000

    Add signup validation

--- server/routes/api/user.route.js
+++ server/routes/api/user.route.js
@@ -1,4 +1,8 @@
 const User = require('../../models/user.model')
+const {
+  validateSignup,
+  validateSignin,
+} = require('../../validation/auth.validator')
 
 const initEndpoints = router => {
   router.get('/user', async (req, res) => {
@@ -6,11 +10,21 @@ const initEndpoints = router => {
   })
 
   router.post('/user', async (req, res) => {
+    const { email } = req.body
+
+    const { error } = validateSignup(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // Check if user is already in database
+    const isInUse = await User.findOne({ email })
+    if (isInUse)
+      return res.status(400).json({ message: 'Email already in use' })
+
     try {
-      const user = await new User({ email: req.body.email }).save()
+      const user = await new User({ email }).save()
       res.json(user)
     } catch (err) {
-      res.json({ message: err })
+      res.status(400).json({ message: err })
     }
   })
 }
