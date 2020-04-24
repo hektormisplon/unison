adffe20c6608ffee1e4b01505abc44214479947e
Hektor Misplon <hektor.misplon@gmail.com>
2020-04-24 00:38:02 +0000

    Add login

--- server/routes/api/auth.route.js
+++ server/routes/api/auth.route.js
@@ -17,9 +17,8 @@ Signup*/
     if (error) return res.status(400).send(error.details[0].message)
 
     // Check if user is already in database
-    const isInUse = await User.findOne({ email })
-    if (isInUse)
-      return res.status(400).json({ message: 'Email already in use' })
+    const user = await User.findOne({ email })
+    if (user) return res.status(400).json({ message: 'Email already in use' })
 
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
@@ -30,6 +29,27 @@ Signup*/
     } catch (err) {
       res.status(400).json({ message: err })
     }
+  })
+
+  /*
+Signin */
+
+  router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+
+    const { error } = validateSignin(req.body)
+    if (error) return res.status(404).send(error.details[0].message)
+
+    // Check if user in database
+    const user = await User.findOne({ email })
+    if (!user) return res.status(400).send('Invalid email or password')
+
+    // Check password
+    const isCorrectPassword = await bcrypt.compare(password, user.password)
+    if (!isCorrectPassword)
+      return res.status(400).send('Invalid email or password')
+
+    res.json({ message: 'You are signed in' })
   })
 }
 
