d74c72c78656c5021dae6977e7676bc90b87d021
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-14 21:28:43 +0000

    Handle 404 & 505 errors

--- server/src/app.js
+++ server/src/app.js
@@ -19,11 +19,16 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(logger)
 
-app.use((req, res, next) => res.status(404).json({ message: 'Not found' }))
-
 /*
 Router */
 
 app.use('/api', apiRouter)
+
+/*
+Handle 404 & 500 */
+app.use((req, res) => res.status(404).json({ message: 'Not found' }))
+app.use((err, req, res, next) =>
+  res.status(500).json({ message: 'Internal server error' }, err)
+)
 
 module.exports.app = app
