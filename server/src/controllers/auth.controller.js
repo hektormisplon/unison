6f3fb3606223c812a163c86bc7985958bd5873b2
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-14 19:29:04 +0000

    Refactor error handling

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt'),
-  jwt = require('jsonwebtoken'),
-  { env } = require('../config')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+const { env } = require('../config')
 const User = require('../models/user.model')
 const {
   validateSignup,
@@ -12,19 +12,20 @@ const {
 Validate request & handle validation error */
 
 exports.signup = async (req, res) => {
-  const { email, password } = req.body
-
-  const { error } = validateSignup(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const user = await User.findOne({ email })
-  if (user) return res.status(400).json({ message: 'Email already in use' })
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
   try {
-    const user = await new User({ email, password: hashedPassword }).save()
+    const { email, password } = req.body
+
+    const { error } = validateSignup(req.body)
+    if (error)
+      return res.status(400).json({ message: error.details[0].message })
+
+    let user = await User.findOne({ email })
+    if (user) return res.status(400).json({ message: 'Email already in use' })
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    user = await new User({ email, password: hashedPassword }).save()
     res.json({ userId: user._id })
   } catch (err) {
     res.status(400).json({ message: err })
@@ -36,18 +37,19 @@ Sign in */
 exports.signin = async (req, res) => {
   const { email, password } = req.body
 
-  const { error } = validateSignup(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  const { error } = validateSignin(req.body)
+  if (error) return res.status(400).json({ message: error.details[0].message })
 
   try {
     // Check if user in database
     const user = await User.findOne({ email })
-    if (!user) return res.status(400).send('Invalid email or password')
+    if (!user)
+      return res.status(400).json({ message: 'Invalid email or password' })
 
     // Check password
     const isCorrectPassword = await bcrypt.compare(password, user.password)
     if (!isCorrectPassword)
-      return res.status(400).send('Invalid email or password')
+      return res.status(400).json({ message: 'Invalid email or password' })
 
     // Assign jwt
     const token = jwt.sign({ id: user._id }, env.JWT_SECRET)
