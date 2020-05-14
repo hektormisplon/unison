400fbf18cc0a66451ccf7693f3f4377ccb00480f
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-14 17:17:08 +0000

    Add basic user crud

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,39 @@
+const User = require('../models/user.model')
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
+}
+
+exports.getUser = async (req, res) => {
+  const { params } = req
+  try {
+    const user = await User.findById(params.id)
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
+}
+
+exports.deleteUsers = async (req, res) => {
+  try {
+    const users = await User.deleteMany()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  const { params } = req
+  try {
+    const user = await User.findOneAndDelete({ _id: params.id })
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
+}
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,11 +1,11 @@
+const userController = require('../controllers/user.controller')
 const auth = require('./auth')
 
 const initEndpoints = router => {
-  router.get('/user', auth, async (req, res) => {
-    req.log.info('user request')
-    const { user } = req
-    res.json(user)
-  })
+  router.get('/user', auth, userController.getUsers)
+  router.get('/user:id', auth, userController.getUser)
+  router.delete('/user', auth, userController.deleteUsers)
+  router.delete('/user:id', auth, userController.deleteUser)
 }
 
 module.exports = initEndpoints
