92269fe21f517d2b55ae8ce1901cfc7a0c297be7
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-15 13:44:47 +0000

    Fix typo

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const controller = require('../controllers/auth.controller')
 
-module.exorts = router => {
+module.exports = router => {
   router.post('/signup', controller.signup)
   router.post('/signin', controller.signin)
 }
